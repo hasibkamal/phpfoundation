49f3c37b572d925e88d9d89a7b7e32706d086b67
ishrat009 <ishrat.cste@gmail.com>
2014-07-09 14:22:21 +0600

    elseif added

---
+++ ifelse.php
@@ -0,0 +1,33 @@
+<?php
+
+$marks=43;
+
+if($marks>79 && $marks<=100)
+{
+    echo "Grade: A+";
+}
+elseif($marks>69 && $marks<80)
+{
+    echo "Grade: A";
+}
+elseif($marks>59 && $marks<70)
+{
+    echo "Grade: A-";
+}
+elseif($marks>49 && $marks<60)
+{
+    echo "Grade: B";
+}
+elseif($marks>39 && $marks<50)
+{
+    echo "Grade: C";
+}
+elseif($marks>69 && $marks<80)
+    {
+    echo "Grade: D";
+    }
+ else
+ {
+     echo "Grade: F";
+ }
+ 
