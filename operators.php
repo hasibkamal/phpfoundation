c4e0b5d04cdd862cdb0f871d827593dcb06e43b2
ishrat009 <ishrat.cste@gmail.com>
2014-07-09 12:15:20 +0600

    operator file added

---
+++ operators.php
@@ -0,0 +1,25 @@
+<?php
+//Macth Operators
+$x = 100/(2*5);
+$y = 100%(2*5);
+$z = 100/2*5;
+$a = 0/5;
+$b = 0%5;
+
+echo $x."<br/>";
+echo $y."<br/>";
+echo $z."<br/>";
+echo $a."<br/>";
+echo $b."<br/><br/>";
+
+//Unary operators
+//increment/decrement
+//pre/post
+$a = 2;
+echo $a++."<br/>";
+echo $a--."<br/>";
+echo ++$a."<br/>";
+echo --$a."<br/><br/>";
+
+echo $a.PHP_EOL;
+echo ++$a;
