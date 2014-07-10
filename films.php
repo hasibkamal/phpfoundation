97f39c5a32de574307327b5a7c38c5226fbfcbda
ishrat009 <ishrat.cste@gmail.com>
2014-07-10 11:46:32 +0600

    films program corrected

--- films.php
+++ films.php
@@ -3,23 +3,61 @@
 $films = array("genres" => array("comedy", "tragedy","action","romance"),
                "film_titles"=>array("Big","Star Wars", "Titanic", "French Kiss"),
                "stars" => array("Bill Murray", "Mark Hammel", "Leonard DiCaprio", "Cate Blanchette"),
-);
+                );
 
 
 //print_r($films);
 
-$genre=array_shift($films);
-//$output=array();
+
 //print_r($genre);
+
+/*$output=array();
 
 foreach($films as $info)
 {
     $newinfo=array();
-    
-    foreach($genre as $index=>$name)
+ 
+    foreach($genre as $film_title>$star)
     {
-        $newinfo[$name]=$info[index];
+        //$newinfo[$name]=$info[index];
+        echo $film_title."-".$star;
+        
     }
-    $output[]=$newinfo;
+    
+  /*$output[]=$newinfo;
 }
-//var_dump($output);
+var_dump($output); */
+
+function info_by_genre($genre)
+{
+$films = array("genres" => array("comedy", "tragedy","action","romance"),
+               "film_titles"=>array("Big","Star Wars", "Titanic", "French Kiss"),
+               "stars" => array("Bill Murray", "Mark Hammel", "Leonard DiCaprio", "Cate Blanchette"),
+                );
+$genre=$films["genres"];
+$film_title= $films["film_titles"];
+$star = $films["stars"];
+if($genre=$genre[1])
+{
+    echo "Film Title: ".$film_title[1]."<br/>";
+    echo "Star: ".$star[1];
+}
+elseif($genre=$genre[2])
+{
+       echo "Film Title: ".$film_title[2]."<br/>";
+    echo "Star: ".$star[2];
+}
+elseif($genre=$genre[3])
+{
+        echo "Film Title: ".$film_title[3]."<br/>";
+    echo "Star: ".$star[3];
+}
+else
+{
+       echo "Film Title: ".$film_title[4]."<br/>";
+    echo "Star: ".$star[4];
+}
+
+}
+
+info_by_genre("comedy");
