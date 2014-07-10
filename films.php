dd37c02ec728d35bb2afb952a0358ec076e13d18
ishrat009 <ishrat.cste@gmail.com>
2014-07-10 14:09:26 +0600

    inputgenre added

--- films.php
+++ films.php
@@ -1,6 +1,6 @@
 <?php
 
-function info_by_genre($genre)
+function info_by_genre($inputgenre)
 {
 $films = array("genres" => array("comedy", "tragedy","action","romance"),
                "film_titles"=>array("Big","Star Wars", "Titanic", "French Kiss"),
@@ -11,17 +11,17 @@ $genre=$films["genres"];
 $film_title= $films["film_titles"];
 $star = $films["stars"];
 
-if($genre==$genre[0])
+if($inputgenre==$genre[0])
 {
     echo "Film Title: ".$film_title[0]."<br/>";
     echo "Star: ".$star[0];
 }
-elseif($genre==$genre[1])
+elseif($inputgenre==$genre[1])
 {
      echo "Film Title: ".$film_title[1]."<br/>";
     echo "Star: ".$star[1];
 }
-elseif($genre==$genre[2])
+elseif($inputgenre==$genre[2])
 {
         echo "Film Title: ".$film_title[2]."<br/>";
     echo "Star: ".$star[2];
@@ -35,4 +35,4 @@ else
 
 }
 
-info_by_genre("tragedy");
+info_by_genre("action"); 
