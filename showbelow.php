d92b51357eec4cb7eeeeebf892f428ca1743ec6f
ishrat009 <ishrat.cste@gmail.com>
2014-07-17 08:35:50 +0600

    Simple program where input will be shown below

--- showbelow.php
+++ showbelow.php
@@ -11,15 +11,15 @@ echo "Id: ".$id."<br/>"."Name: ".$name."<br/>"."Salary: ".$salary;
 }
 
 function show_info_below($id,$name,$salary)
-{
-$id;
-$name;
-$salary;
-echo "<br/>"."<br/>";
-echo "Id: "."<br/>"."Name: "."<br/>"."Salary: ";
-echo "<br/>";
-echo "Id: ".$id.", "."Name: ".$name.", "."Salary: ".$salary;
-}
+  {
+    $id;
+    $name;
+    $salary;
+    echo "<br/>"."<br/>";
+    echo "Id: "."<br/>"."Name: "."<br/>"."Salary: ";
+    echo "<br/>";
+    echo "Id: ".$id.", "."Name: ".$name.", "."Salary: ".$salary;
+  }
 }
 $obj_show_below= new show_below();
 
